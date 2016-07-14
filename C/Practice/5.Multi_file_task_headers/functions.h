9893d4a980882b13f2c3c8d30040b6f7dabc8afc
medva1997 <medva1997@yandex.ru>
2016-07-14 23:13:47 +0300

    remove all files

--- C/Practice/5.Multi_file_task_headers/functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-int expected_value(FILE *f, float *max_of_identical);
-int dispersionfunc(FILE *f,float expectvalue, float *max_of_identical);
-
-#endif // FUNCTIONS_H
-
